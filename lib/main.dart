6cc40995d1807efaa4a83aab52cfad0395c85aeb
Abdulrahman Ibrahim <abdulrahmanibrahim@Abdulrahmans-MacBook-Air.local>
2025-06-29 16:38:13 +0300

    did the splash and onboarding screen and custom button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:glowup/Screens/Shared/Onboarding/onboarding_screen.dart';
+
+import 'package:glowup/Screens/onboarding/onboarding_screen.dart';
+import 'package:glowup/Screens/splash/splash.dart';
 import 'package:glowup/Styles/theme.dart';
 import 'package:glowup/Utilities/setup.dart';
 
@@ -22,8 +24,13 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) =>
-          MaterialApp(theme: lightTheme, darkTheme: darkTheme, home: child),
-      child: const OnboardingScreen(),
+          MaterialApp(theme: lightTheme, darkTheme: darkTheme, initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
+      },),
+    
+      
     );
   }
 }
