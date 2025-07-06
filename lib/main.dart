eb5432b8a6c3837b4b087723bdd1705758fdb9b9
Abdulrahman Ibrahim <abdulrahmanibrahim@Abdulrahmans-MacBook-Air.local>
2025-07-06 13:38:15 +0300

    Merged Solaiman-AlDokhail into lamees and resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
 import 'package:glowup/Repositories/api/supabase_connect.dart';
+import 'package:glowup/Screens/Customer/BookingConfirmation/booking_confirmation_screen.dart';
 import 'package:glowup/Screens/Customer/NavBar/nav_bar_screen.dart';
+
 import 'package:glowup/Screens/Provider/NavBar/provider_nav_bar_screen.dart';
 import 'package:glowup/Screens/Provider/SignUp/provider_sign_up_screen.dart';
 import 'package:glowup/Screens/Shared/Login/login_screen.dart';
