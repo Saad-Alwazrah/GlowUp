afc4c2aad66fce6eeb16735c46c5f931024bd5e9
unknown <saad.mu2001@gmail.com>
2025-07-08 18:06:59 +0300

    Finished Employees

--- lib/Screens/Provider/EmployeeDetails/employee_details.dart
+++ lib/Screens/Provider/EmployeeDetails/employee_details.dart
@@ -1,13 +1,19 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glowup/CustomWidgets/Shared/custom_elevated_button.dart';
+import 'package:glowup/CustomWidgets/shared/custom_background_container.dart';
+import 'package:glowup/Repositories/models/stylist.dart';
 import 'package:glowup/Screens/Provider/EmployeeDetails/bloc/employee_details_bloc.dart';
 import 'package:glowup/Styles/app_colors.dart';
+import 'package:glowup/Styles/app_font.dart';
 import 'package:glowup/Utilities/extensions/screen_size.dart';
 
 class EmployeeDetails extends StatelessWidget {
-  const EmployeeDetails({super.key});
+  final Stylist stylist;
+  const EmployeeDetails({super.key, required this.stylist});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +23,6 @@ class EmployeeDetails extends StatelessWidget {
         builder: (context) {
           final bloc = context.read<EmployeeDetailsBloc>();
           return Scaffold(
-            // The Image of the Provider in the background of the AppBar
             body: Column(
               children: [
                 Container(
@@ -30,6 +35,42 @@ class EmployeeDetails extends StatelessWidget {
                         Icon(Icons.image, color: AppColors.white),
                   ),
                 ),
+                SizedBox(height: 64.h),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(width: 120.w),
+                    Text(stylist.name, style: AppFonts.semiBold24),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        children: [
+                          stylist.avgRating == null
+                              ? Text("No Rating".tr())
+                              : Text("${stylist.avgRating}"),
+                          Icon(Icons.star, color: Colors.yellow.shade600),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 48.h),
+
+                CustomBackgroundContainer(
+                  childWidget: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(stylist.bio!, style: AppFonts.medium18),
+                  ),
+                  height: 150,
+                ),
+
+                // CustomCalendar(stylist: stylist, onDaySelected: onDaySelected, currentDay: currentDay)
+                CustomElevatedButton(
+                  text: "Edit ",
+                  onTap: () {},
+                ), // Editing the schedule
               ],
             ),
           );
